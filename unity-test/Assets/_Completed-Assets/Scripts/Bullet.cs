d400dc5a34035eabc7ca651e9a8efad9a8276c79
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-07-22 08:47:53 +0900

    update

--- unity-test/Assets/_Completed-Assets/Scripts/Bullet.cs
+++ unity-test/Assets/_Completed-Assets/Scripts/Bullet.cs
@@ -1,25 +1,11 @@
 ﻿using UnityEngine;
 
-namespace CompletedAssets
+public class Bullet : MonoBehaviour
 {
-	public class Bullet : MonoBehaviour
-	{
-		// 弾の移動スピード
-		public int speed = 10;
+  public int speed = 10;
 
-		// ゲームオブジェクト生成から削除するまでの時間
-		public float lifeTime = 1;
-
-		// 攻撃力
-		public int power = 1;
-
-		void Start ()
-		{
-			// ローカル座標のY軸方向に移動する
-			GetComponent<Rigidbody2D> ().velocity = transform.up.normalized * speed;
-		
-			// lifeTime秒後に削除
-			Destroy (gameObject, lifeTime);
-		}
-	}
+  void Start ()
+  {
+    GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+  }
 }

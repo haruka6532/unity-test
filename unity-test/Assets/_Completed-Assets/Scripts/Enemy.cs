e19b00ca7f24519882d024c3ac502e748ce77442
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-07-22 23:32:19 +0900

    update

---
+++ unity-test/Assets/_Completed-Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Enemy : MonoBehaviour
+{
+  // Spaceshipコンポーネント
+  Spaceship spaceship;
+
+  IEnumerator Start ()
+  {
+    // Spaceshipコンポーネントを取得
+    spaceship = GetComponent<Spaceship> ();
+
+    // ローカル座標のY軸のマイナス方向に移動する
+    spaceship.Move (transform.up * -1);
+
+    if(spaceship.canShot == false){
+        yield break;
+    }
+
+    while (true) {
+
+      // 子要素を全て取得する
+      for (int i = 0; i < transform.childCount; i++) {
+
+        Transform shotPosition = transform.GetChild(i);
+
+        // ShotPositionの位置/角度で弾を撃つ
+        spaceship.Shot (shotPosition);
+      }
+
+      // shotDelay秒待つ
+      yield return new WaitForSeconds (spaceship.shotDelay);
+    }
+  }
+}
--- unity-test/Assets/_Completed-Assets/Scripts/Player.cs
+++ unity-test/Assets/_Completed-Assets/Scripts/Player.cs
@@ -3,20 +3,22 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-  // 移動スピード
-  public float speed = 5;
 
-  // PlayerBulletプレハブ
-  public GameObject bullet;
+  // Spaceshipコンポーネント
+  Spaceship spaceship;
 
-  // Startメソッドをコルーチンとして呼び出す
   IEnumerator Start ()
   {
+    // Spaceshipコンポーネントを取得
+    spaceship = GetComponent<Spaceship> ();
+
     while (true) {
+
       // 弾をプレイヤーと同じ位置/角度で作成
-      Instantiate (bullet, transform.position, transform.rotation);
-      // 0.05秒待つ
-      yield return new WaitForSeconds (0.05f);
+      spaceship.Shot (transform);
+
+      // shotDelay秒待つ
+      yield return new WaitForSeconds (spaceship.shotDelay);
     }
   }
 
@@ -31,7 +33,7 @@ public class Player : MonoBehaviour
     // 移動する向きを求める
     Vector2 direction = new Vector2 (x, y).normalized;
 
-    // 移動する向きとスピードを代入する
-    GetComponent<Rigidbody2D>().velocity = direction * speed;
+    // 移動
+    spaceship.Move (direction);
   }
 }
